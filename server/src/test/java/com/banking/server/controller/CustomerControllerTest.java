daecfa35f72b9e636212972e237a8235240de2f7
harshithapasupuleti <harshithapasupuleti168@gmail.com>
2023-09-13 16:28:22 +0530

    Added Test Cases for Customer controller

---
+++ server/src/test/java/com/banking/server/controller/CustomerControllerTest.java
@@ -0,0 +1,85 @@
+package com.banking.server.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.springframework.http.MediaType;
+
+import com.banking.server.entity.Customer;
+import com.banking.server.entity.LoginModel;
+import com.banking.server.service.CustomerService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(CustomerController.class)
+public class CustomerControllerTest {
+	
+	@Autowired
+	private MockMvc mvc;
+	
+	@MockBean
+	private CustomerService customerService;
+	
+	
+	private static ObjectMapper mapper = new ObjectMapper();
+	
+	@Test
+	public void createCustomerTest() throws Exception {
+		Customer customer = new Customer();
+		customer.setFirstName("Vani");
+		customer.setCustomerId(1);
+		customer.setAadhar("123456789087");
+		customer.setCity("Guntur");
+		customer.setDob("23-09-2002");
+		customer.setEmail("vani@gmail.com");
+		customer.setLastName("Sree");
+		customer.setMobile("9876543210");
+		customer.setOccupation("Engineer");
+		customer.setPassword("vani@123");
+		customer.setSalary(20000.00);
+		customer.setState("AP");
+		Mockito.when(customerService.createCustomer(ArgumentMatchers.any())).thenReturn(customer);
+		String json = mapper.writeValueAsString(customer);
+		mvc.perform(post("/customer").
+				contentType(MediaType.APPLICATION_JSON).characterEncoding("utf-8")
+				.content(json).accept(MediaType.APPLICATION_JSON)).andExpect(status().isCreated());
+	}
+	
+	@Test
+	public void validateCustomerTest() throws Exception{
+		LoginModel l = new LoginModel();
+		l.setCustomerId((long) 1);
+		l.setPassword("random@123");
+		String json = mapper.writeValueAsString(l);
+		Mockito.when(customerService.validateCustomer(ArgumentMatchers.any())).thenReturn("Login Succes");
+		mvc.perform(post("/customer/login").
+				contentType(MediaType.APPLICATION_JSON).characterEncoding("utf-8")
+				.content(json).accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk());
+		
+		
+	}
+	
+	@Test
+	public void getCustomerTest() throws Exception{
+		Customer c = new Customer();
+		Mockito.when(customerService.getCustomer(ArgumentMatchers.anyInt())).thenReturn(c);
+		mvc.perform(get("/customer/{id}",1)
+			      .contentType(MediaType.APPLICATION_JSON))
+			      .andExpect(status().isBadRequest());
+		
+	}
+
+}
+
+
