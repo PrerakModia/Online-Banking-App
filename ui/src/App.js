1f1f6144adf06a60b81b6cd0e20abe0e613b696d
PrerakModia <88586985+PrerakModia@users.noreply.github.com>
2023-09-13 21:48:02 +0530

    Feature: Tailwind + LogIn Page

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,32 +1,33 @@
-import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { ThemeProvider, createTheme } from "@mui/material/styles";
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 
-import HomePage from "./pages/HomePage";
-import AccountPage from "./pages/AccountPage";
-import Transactions from "./pages/Transactions";
-import Nav from "./components/NavBarComponent";
-import Error from "./pages/ErrorPage";
-import CreateAccount from "./pages/CreateAccount";
+import HomePage from './pages/HomePage';
+import AccountPage from './pages/AccountPage';
+import Transactions from './pages/Transactions';
+import LoginPage from './components/LoginPage';
+import Nav from './components/NavBarComponent';
+import Error from './pages/ErrorPage';
+import CreateAccount from './pages/CreateAccount';
 function App() {
   const lightTheme = createTheme({
     palette: {
-      type: "light",
+      type: 'light',
       primary: {
-        light: "#fff",
-        main: "#Ff0000",
-        dark: "#004d40",
+        light: '#fff',
+        main: '#Ff0000',
+        dark: '#004d40',
       },
     },
   });
 
   return (
     <ThemeProvider theme={lightTheme}>
-      <div className="App">
-        <Nav />
+      <div>
+        {/* <Nav /> */}
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<HomePage />} />
+            <Route path="/" element={<LoginPage />} />
             <Route path="/account" element={<AccountPage />} />
             <Route path="/transaction" element={<Transactions />} />
             <Route path="/createaccount" element={<CreateAccount />} />
